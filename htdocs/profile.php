8a4e0e75099d6eb62abcb85ed680310e4af56021
mukko <mukk0.aquarius@gmail.com>
2015-06-12 16:42:49 +0900

    プロフィールの枠組みを作成

---
+++ htdocs/profile.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <link type="text/css" href="css/profile.css" rel="stylesheet">
+  <font face="HGP創英角ﾎﾟｯﾌﾟ体","ヒラギノ角ゴ Pro W3","メイリオ">
+    <title>profile</title>
+  </head>
+  <body>
+
+    <div id="wrapper">
+      <!-- ページ本体 -->
+      <div id="container">
+        <!-- ヘッダー -->
+        <div id="header">
+          header
+        </div>
+        <div id="profile">
+          <!-- プロフィール -->
+          <div id="my_column">
+            <div id="column">
+              <h1>profile</h1>
+              flkjdoi;weanoae
+            </div>
+          </div>
+          <!-- コンテンツ -->
+          <div id="my_contents">
+            <div id="contents">
+              <h1>aaaaaaaa</h1>
+              fjeklw;ajfwoeai;fgnwaogtaw;oitgo
+            </div>
+          </div>
+        </div>
+        <div id="footer">
+          footer
+        </div>
+      </div>
+
+    </div>
+  </font>
+</body>
+</html>
