42792975df4123c6bf229bba79bbafc280690154
Torimura Takanobu <000ubird@gmail.com>
2015-07-17 15:18:59 +0900

    サインアップ処理を実装

---
+++ signup_form.php
@@ -0,0 +1,18 @@
+﻿<?php
+	//共通部分の読み込み
+	require_once("common.php");
+	
+	//Smartyオブジェクト作成
+	$smarty = new Smarty();
+	
+	//ひな形のSmartyテンプレートでincludeするテンプレートを指定
+	$smarty->assign('content_tpl', 'signup_form.tpl');
+	
+	$smarty->assign('params',array(
+		'types' => array()
+		)
+	);
+	
+	//ページを表示する
+	$smarty->display('signup_common.tpl');
+?>
