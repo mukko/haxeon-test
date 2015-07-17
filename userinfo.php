d6530a2b037f4588e798683e0dd8f58ea5083009
mukko <mukk0.aquarius@gmail.com>
2015-07-17 18:08:24 +0900

    bug fix

--- userinfo.php
+++ userinfo.php
@@ -6,6 +6,15 @@ if(!isset($_SESSION['userID']))
 	echo '{ "error":"not login"}';
 	exit;
 }
+
+if(!isset($_SESSION['projectName']))
+{
+	echo '{ "userID":"' . $_SESSION['userID'] . '",
+					"projectName":""
+	}';
+	exit;
+}
+
 echo '{ "userID":"' . $_SESSION['userID'] . '",
 				"projectName":"' . $_SESSION['projectName'] . '"
 }';
