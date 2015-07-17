11f01571f4a8d2cf2d129fd86582ef2b76a8e263
mukko <mukk0.aquarius@gmail.com>
2015-07-17 14:48:58 +0900

    プロジェクト作成画面を作成。ログインしているユーザーで重複したプロジェクト名ならば元の作成画面に戻る処理を作成。
    以前のプロジェクト作成画面を削除

--- create_post.php
+++ create_post.php
@@ -1,6 +1,19 @@
 <?php
   session_start();
-  $_SESSION['projectName'] = $_POST['projectName'];
-  header('location: try-haxe/index.html');
-  exit();
+
+  //データベース接続処理
+	$dbName = 'haxeon';
+	$user = 'root';
+	$password = 'DELL';
+	$db = new mysqli('localhost', $user , $password, $dbName) or die("error");
+
+  $result = $db->query('SELECT * FROM `project` WHERE `ownerUserID` = "'.$_SESSION['userID'].'" AND `projectName` = "'.$_POST['projectName'].'"');
+  if($result->num_rows == 0){
+    $_SESSION['projectName'] = $_POST['projectName'];
+    header('location: try-haxe/index.html');
+    exit();
+  }else{
+    header('location: create_form.php');
+    exit();
+}
 ?>
