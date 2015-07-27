16f4de590e7d9c06cae42eb1135fe144cd85a021
Torimura Takanobu <000ubird@gmail.com>
2015-07-27 15:59:07 +0900

    アカウント認証ページを作成
    メールのリンクに記載されているハッシュ値とデータベースのハッシュ値が一致した場合、アカウントを有効にする処理を実装。

--- authentication.php
+++ authentication.php
@@ -2,10 +2,60 @@
 	//共通部分の読み込み
 	require_once("common.php");
 	
-	//URLに前のフォームで入力した情報があれば登録する
-	if (isset($_GET['hash'])) {
-		$hash = $_GET['hash'];
-		$id = $_GET['id'];
-		echo $hash;
-		echo $id;
+	require_once('Smarty.class.php');
+	$smarty = new Smarty();
+	
+	$id = "";
+	$hash = "";
+	$smarty->assign('isEnable', false);
+	
+	//データベース接続処理
+	$dbName = 'haxeon';
+	$user = 'root';
+	$password = 'DELL';
+	$db = new mysqli('localhost', $user , $password, $dbName) or die("error");
+
+	//データベースへの接続が失敗したらエラーを出力して終了
+	if ($db->connect_error){
+		error("データベースの接続に失敗しました。申し訳ございませんが、後ほどやり直して下さい。",$smarty);
 	}
+	
+	//URLに前のフォームで入力した情報があれば登録する
+	if (isset($_GET['hash']) && isset($_GET['id'])) {
+		$id = $_GET['id'];
+		$hash = $_GET['hash'];
+	}
+	else {
+		error("認証に失敗しました。メールのリンクをご確認ください。",$smarty);
+	}
+	
+	//データベースのハッシュと照合
+	$result = $db->query("SELECT * FROM `account` WHERE `userID`= '$id';");
+	if ($result) {
+		while ($row = $result->fetch_object()) {
+			$db_hash = htmlspecialchars($row->MD5);
+		}
+		//成功した場合はアカウントを有効にする
+		if ($db_hash == $hash) {
+			$smarty->assign('isEnable', true);
+			$db->query("UPDATE `account` SET `isEnable` = 1 WHERE `userID` = '$id';");
+		}
+		else {
+			error("認証に失敗しました。メールのリンクをご確認ください。",$smarty);
+		}
+	}
+	else {
+		error("認証に失敗しました。メールのリンクをご確認ください。",$smarty);	
+	}
+	
+	$db->close();
+	
+	$smarty->display('authentication.tpl');
+
+	
+//エラー出力関数
+function error($errorMsg, $smarty) {
+	$smarty->assign('errorMsg', $errorMsg);
+	$smarty->display('authentication.tpl');
+	exit();	
+}
---
+++ authentication.tpl
@@ -0,0 +1,22 @@
+﻿<html>
+	<head><title>アカウント認証</title></head>
+
+	<body>
+	<font face="ヒラギノ角ゴ Pro W3","メイリオ">
+
+	<!-- 認証が成功した場合　-->
+	{if $isEnable}
+		<font size="70" face="メイリオ">
+			認証が成功しました!!<br>
+			ログイン画面に移動します。
+			<meta http-equiv="refresh" content="3;URL=http://localhost/haxeon/login_form.php">
+		</font>
+	<!-- 誤った内容が入力された場合　-->
+	{else}
+		<font size="70" face="メイリオ">{$errorMsg}</font>
+	{/if}
+	
+	</font>
+	</body>
+	
+</html>
