d6bc703855c0a1c8832a7e5ea9ccaad93e5e5835
Torimura Takanobu <000ubird@gmail.com>
2015-08-03 17:27:05 +0900

    スマーティ処理共通クラスのコンストラクタの引数を変更
    可変引数からリストに変更。

--- AssignSmarty.php
+++ AssignSmarty.php
@@ -8,27 +8,24 @@ class AssignSmarty {
 	private $smarty;
 	private $dispTpl;
 	
-	//スマーティにアサインする可変引数以外の引数の数
-	private static $DEFAULT_NUM_ARGS;
-	
 	/**コンストラクタでテンプレートの読み込み、Smartyパラメータのアサイン、ページの表示を行う。
 	* $common : 共通部分のPHPファイル名
 	* $dispTpl: 描画するテンプレートファイル名
-	* $smartyArgs : 偶数番目には割り当て名、奇数番目には値を入れる
+	* $smartyArgs : Smartyにアサインする変数を持つリスト。偶数番目には割り当て名、奇数番目には値を入れる
 	*/
-	function __construct($common, $dispTpl, ...$smartyArgs) {
+	function __construct($common, $dispTpl, $smartyArgs) {
 		//共通テンプレートの読み込み
 		$this->common = $common;
 		require_once($this->common);
 		
 		//Smartyオブジェクト生成
+		require_once('Smarty.class.php');
 		$smarty = new Smarty();
 		
 		//Smartyにパラメータをアサイン
-		$DEFAULT_NUM_ARGS = 2;
-		for($i=$DEFAULT_NUM_ARGS; $i < func_num_args(); $i++){
-			$smartyArgsName = func_get_arg($i++);
-			$smartyArgsValue= func_get_arg($i);
+		for($i=1; $i<count($smartyArgs); $i++) {
+			$smartyArgsName = $smartyArgs[$i++];
+			$smartyArgsValue= $smartyArgs[$i];
 			$smarty->assign($smartyArgsName, $smartyArgsValue);
 		}
 		
