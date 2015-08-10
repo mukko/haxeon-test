56971eb1f089e7bb25c298f7db064f3571a58d43
Torimura Takanobu <000ubird@gmail.com>
2015-08-10 11:04:54 +0900

    プロジェクトランキングを取得するSQLクエリを変更

--- index.php
+++ index.php
@@ -3,9 +3,28 @@
 
 	require_once('Smarty.class.php');
 	$smarty = new Smarty();
-
+	
+	$days = "1day";
+	$order = "pv";
+	$page = 1;
+	
+	if (isset($_GET['days'])) {
+		$days = $_GET['days'];
+	}
+	if (isset($_GET['order'])) {
+		$order = $_GET['order'];
+	}
+	if (isset($_GET['page'])) {
+		$page = $_GET['page'];
+	}
+	
 	//ホットコードを表示
-	$result = $db->query("SELECT * FROM `PROJECT` ORDER BY `PV` DESC");
+	$result = $db->query("
+		SELECT * FROM project 
+		WHERE project.modified BETWEEN '2015-08-05' AND '2015-08-06'
+		ORDER BY project.pv 
+		DESC LIMIT 30");
+		
 	if($result){
 		$i = 0;
 		$array = [];
